33c896ebacd2edf05edeaf73d672e9878221267d
Raul Santos <raulbolsantos@gmail.com>
2019-09-12 14:22:30 -0300

    Refactor to arrow functions

--- why-frameworks-matter - 0/hackernews/src/react/index.js
+++ why-frameworks-matter - 0/hackernews/src/react/index.js
@@ -1,12 +1,8 @@
 const BASE_URL = 'https://hn.algolia.com/api/v1/';
 
 function doSearch(query) {
-    const URL = BASE_URL + 'search?query=' + query + '&hitsPerPage=200';
-    return fetch(URL)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (result) {
-            return result.hits;
-        });
+    const url = BASE_URL + 'search?query=' + query + '&hitsPerPage=200';
+    return fetch(url)
+        .then(response => response.json())
+        .then(result => result.hits);
 }
