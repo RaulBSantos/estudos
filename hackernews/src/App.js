a3ba85ca1068b9581f653ff33acf2ab602b7896c
Raul Santos <raulbolsantos@gmail.com>
2019-11-21 15:37:46 -0300

    Add gitignore and treats list elements

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -1,20 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+
+// Declare a list (mock to API returns)
+const list = [
+  {
+    title: 'React',
+    url: 'https://facebook.github.io/react/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
 // function App() {
 // App is a COMPONENT
 class App extends Component {
   render() {
-    // Everythin inside the render method is a ELEMENT
-    const helloWorld = 'Welcome to the Road to learn React';
-    const people = { name: 'Raul', lastName: 'Bolsonaro' };
+    // Everything inside the render method is a ELEMENT
     return (
-      < div className="App" >
-        {/* You can put JS code here */}
-        <h2>{helloWorld}</h2>
-        <p>Hello {people.name}. Your lastname is {people.lastName}</p>
-      </div >
+      <div className="App">
+        {list.map((item) => {
+          return (
+            <div key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+            </div>
+          );
+        })}
+      </div>
     );
   }
   // }
