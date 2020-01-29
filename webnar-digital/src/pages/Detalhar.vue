9234362862c5285ccb9bd17b89b510e42effdf08
Raul Santos <raulbolsantos@gmail.com>
2020-01-29 18:51:04 -0300

    Text to speech feature

--- webnar-digital/src/pages/Detalhar.vue
+++ webnar-digital/src/pages/Detalhar.vue
@@ -1,5 +1,10 @@
 <template>
-  <q-page padding><div v-html="texto"></div></q-page>
+  <q-page padding>
+    <div v-html="texto"></div>
+    <q-page-sticky position="top-right" :offset="[18, 18]">
+      <q-btn round color="negative" icon="play_arrow" @click="sintetizarFala()" />
+    </q-page-sticky>
+  </q-page>
 </template>
 
 <script>
@@ -8,6 +13,11 @@ export default {
   props: {
     texto: {
       type: String
+    }
+  },
+  methods: {
+    sintetizarFala() {
+      this.$speechTalk(window.getSelection().toString());
     }
   }
 };
--- webnar-digital/src/pages/Index.vue
+++ webnar-digital/src/pages/Index.vue
@@ -1,16 +1,9 @@
 <template>
   <q-page class="container">
     <div class="row q-col-gutter-sm">
-      <div
-        class="col-md-3 col-sm-4"
-        v-for="(post, index) in posts"
-        :key="index"
-      >
+      <div class="col-md-3 col-sm-4" v-for="(post, index) in posts" :key="index">
         <q-card class="my-card" style="min-height: 23rem">
-          <img
-            :src="post.jetpack_featured_media_url"
-            style="max-height: 200px"
-          />
+          <img :src="post.jetpack_featured_media_url" style="max-height: 200px" />
 
           <q-card-section>
             <div class="text-h6">{{ post.title.rendered }}</div>
@@ -21,8 +14,7 @@
               color="secondary"
               class="full-width"
               @click="detalhar(post.content.rendered)"
-              >Detalhar</q-btn
-            >
+            >Detalhar</q-btn>
           </div>
         </q-card>
       </div>
