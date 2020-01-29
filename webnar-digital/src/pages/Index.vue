0c4799576270e09795fd0fc91c2ef5f380156a6f
Raul Santos <raulbolsantos@gmail.com>
2020-01-29 13:57:30 -0300

    Initial quasar config running

---
+++ webnar-digital/src/pages/Index.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-page class="container">
+    <div class="row">
+      <div class="col-3">
+        <q-card class="my-card">
+          <img src="https://cdn.quasar.dev/img/mountains.jpg" />
+
+          <q-card-section>
+            <div class="text-h6">Our Changing Planet</div>
+            <div class="text-subtitle2">by John Doe</div>
+          </q-card-section>
+
+          <q-card-section class="q-pt-none"> </q-card-section>
+        </q-card>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "PageIndex"
+};
+</script>
