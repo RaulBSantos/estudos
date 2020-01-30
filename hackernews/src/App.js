6bcd38f261e939361202ac3a778fad5ed33a5605
Raul Santos <raulbolsantos@gmail.com>
2020-01-29 21:28:52 -0300

    Adds moment-js and Timer

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -1,46 +1,54 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import moment from "moment";
 
-
-// Declare a list (mock to API returns)
 const list = [
   {
-    title: 'React',
-    url: 'https://facebook.github.io/react/',
-    author: 'Jordan Walke',
+    title: "React",
+    url: "https://facebook.github.io/react/",
+    author: "Jordan Walke",
     num_comments: 3,
     points: 4,
-    objectID: 0,
+    objectID: 0
   },
   {
-    title: 'Redux',
-    url: 'https://github.com/reactjs/redux',
-    author: 'Dan Abramov, Andrew Clark',
+    title: "Redux",
+    url: "https://github.com/reactjs/redux",
+    author: "Dan Abramov, Andrew Clark",
     num_comments: 2,
     points: 5,
-    objectID: 1,
-  },
+    objectID: 1
+  }
 ];
+
+class Timer {
+  constructor() {
+    this.current = this.clock();
+  }
+  clock() {
+    return moment().format("DD/MM/YYYY, HH:mm:ss");
+  }
+}
 
 // function App() {
 // App is a COMPONENT
 class App extends Component {
   render() {
-    // Everything inside the render method is a ELEMENT
+    const timer = new Timer();
+    // Everythin inside the render method is a ELEMENT
     return (
       <div className="App">
-        {list.map((item) => {
-          return (
-            <div key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-            </div>
-          );
-        })}
+        <span>Data: {timer.current}</span>
+        {list.map(item => (
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </div>
+        ))}
       </div>
     );
   }
--- hackernews/src/index.js
+++ hackernews/src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
 // This is an INSTANCE of 'App' Component
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // Sets configuration to update content without reload the page after a save
 if (module.hot) {
-    module.hot.accept();
+  module.hot.accept();
 }
 
 // If you want your app to work offline and load faster, you can change
